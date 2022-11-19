97eeba4dd5e61bc35c330db00c1590ec72f408d4
Ralph Schlosser <bwv988@users.noreply.github.com>
2022-11-19 13:39:08 +0000

    Test case finished

--- catalyst_extensions_dev/src/catalyst_tests.scala
+++ catalyst_extensions_dev/src/catalyst_tests.scala
@@ -6,6 +6,7 @@
 // https://www.youtube.com/watch?v=IlovS-Y7KUk - Extension points API
 // https://medium.com/@pratikbarhate/extending-apache-spark-catalyst-for-custom-optimizations-9b491efdd24f
 // Extension points: https://www.databricks.com/blog/2015/04/13/deep-dive-into-spark-sqls-catalyst-optimizer.html
+// https://github.com/apache/spark/blob/master/sql/core/src/test/scala/org/apache/spark/sql/SparkSessionExtensionSuite.scala
 
 // GOALS:
 // 
@@ -13,16 +14,17 @@
 // (2) Add counts for that field to query
 // (3) Prevent returning results if resultset row count below sensitive threshold
 
-val df = spark.read.format("csv").option("header", "true").load("resources/data.csv")
+val df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("resources/data.csv")
 
 df.createOrReplaceTempView("data")
 
 val sqlQuery = """
 select
     data_subject,
-    count(distinct data_subject) as total_txn,
+    count(data_subject) as total_txn,
     sum(amount) as total_amount
 from data
+group by 1
 """
 
 val result = spark.sql(sqlQuery)
